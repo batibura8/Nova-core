25ab38d933438cfbec55412ab9a85ba69818385a
batibura8 <batibura8@gmail.com>
2026-08-01 03:33:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -109,3 +109,14 @@ copyOutput.addEventListener("click", function () {
     navigator.clipboard.writeText(outputText.value);
     alert("Copied! ✅");
 });
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeBtn.textContent = "☀️ Light Mode";
+    } else {
+        themeBtn.textContent = "🌙 Dark Mode";
+    }
+});
